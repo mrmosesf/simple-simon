2ec034382139089c44262f85e52c0c67583dfcd4
Moses Franco <mosesf@outlook.com>
2017-05-01 11:08:21 -0500

    Added another function, and began sequence generating function

--- js/main.js
+++ js/main.js
@@ -8,26 +8,47 @@
     // Global variables
     var gameVars = {
         sequence: [],
-        round: 0
+        round: 0,
+        status: ""
 
     };
-    
+
     var userEvents = [];
 
 
+    // Start game trigger
+    startGame();
+
     // TODO: changes part of game UI, and calls the
     function startGame() {
-        
+        $('#startGame').click(function () {
+            $('#startGame, #currentRound').toggleClass('hidden');
+            roundStart();
+        });
     }
-    
+
+    // TODO: Start a new round, by updating round counter and game message
+    function roundStart() {
+        gameVars.status = "Watch";
+        $('#gameMessage').html(gameVars.status);
+
+        gameVars.round += 1;
+        $('#currentRound').html("Round: " + gameVars.round);
+
+        randomGenerator();
+    }
+
+
     // TODO: Adds a random number that will be added to the game sequence, not deleting it
     function randomGenerator() {
-        
+        var randomSequence = Math.floor(Math.random() * (4 - 1 + 1) +1);
+        gameVars.sequence.push(randomSequence);
+        lightSquares(gameVars.sequence);
     }
 
     // TODO: Highlights squares, based on a variable that's taken in
     function lightSquares(colorSequence) {
-
+        console.log(colorSequence);
     }
 
     // TODO: Checks against user inputted sequence, and already generated numbers
