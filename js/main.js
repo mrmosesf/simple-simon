49f54173d444ea25599a38c1e6a29add4b772436
Moses Franco <mosesf@outlook.com>
2017-05-01 10:15:09 -0500

    Pseudo coded some basics

--- js/main.js
+++ js/main.js
@@ -1,3 +1,48 @@
 /**
  * Created by mosesfranco on 4/28/17.
  */
+
+(function () {
+    "use strict";
+
+    // Global variables
+    var gameVars = {
+        sequence: [],
+        round: 0
+
+    };
+    
+    var userEvents = [];
+
+
+    // TODO: changes part of game UI, and calls the
+    function startGame() {
+        
+    }
+    
+    // TODO: Adds a random number that will be added to the game sequence, not deleting it
+    function randomGenerator() {
+        
+    }
+
+    // TODO: Highlights squares, based on a variable that's taken in
+    function lightSquares(colorSequence) {
+
+    }
+
+    // TODO: Checks against user inputted sequence, and already generated numbers
+    function gameStweard() {
+
+    }
+
+    // TODO: Adds a number to round counter, and begins UI again, possibly clearing user entered events
+    function userSuccess() {
+
+    }
+
+    // TODO: Resets UI to default and setup game to be started again
+    function userFail() {
+
+    }
+
+})();
