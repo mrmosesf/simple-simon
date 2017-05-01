b9386df684263b124aaa49083b03c9f5796545ce
Moses Franco <mosesf@outlook.com>
2017-05-01 11:42:47 -0500

    Added color animation, based on color sequence in array, animated with the JQuery Color plugin

--- js/main.js
+++ js/main.js
@@ -9,7 +9,8 @@
     var gameVars = {
         sequence: [],
         round: 0,
-        status: ""
+        status: "",
+        lockout: true
 
     };
 
@@ -41,14 +42,51 @@
 
     // TODO: Adds a random number that will be added to the game sequence, not deleting it
     function randomGenerator() {
-        var randomSequence = Math.floor(Math.random() * (4 - 1 + 1) +1);
+        var randomSequence = Math.floor(Math.random() * (4 - 1 + 1) + 1);
         gameVars.sequence.push(randomSequence);
+
         lightSquares(gameVars.sequence);
     }
 
     // TODO: Highlights squares, based on a variable that's taken in
-    function lightSquares(colorSequence) {
-        console.log(colorSequence);
+    function lightSquares(colors) {
+        console.log(colors);
+        colors.forEach(function (p1) {
+            switch (p1) {
+                case 1:
+                    console.log(1);
+                    $('#one').animate({
+                        "background-color": "rgba(255, 0, 0, 0.5)"
+                    }, 100).animate({
+                        "background-color": "red"
+                    });
+                    break;
+                case 2:
+                    console.log(2);
+                    $('#two').animate({
+                        "background-color": "rgba(0, 0, 255, 0.5)"
+                    }, 100).animate({
+                        "background-color": "blue"
+                    });
+                    break;
+                case 3:
+                    console.log(3);
+                    $('#three').animate({
+                        "background-color": "rgba(0, 255, 0, 0.5)"
+                    }, 100).animate({
+                        "background-color": "green"
+                    });
+                    break;
+                case 4:
+                    console.log(4);
+                    $('#four').animate({
+                        "background-color": "rgba(255, 255, 0, 0.5)"
+                    }, 100).animate({
+                        "background-color": "yellow"
+                    });
+                    break;
+            }
+        })
     }
 
     // TODO: Checks against user inputted sequence, and already generated numbers
